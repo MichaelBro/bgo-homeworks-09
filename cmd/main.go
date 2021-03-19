663d70124be13543c0363c4cbc8d1ebe500c1f2d
MikhailBrovko <michael.bro@mail.ru>
2021-03-19 16:24:28 +0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+const CRLF = "\r\n"
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:9999")
+
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		if cerr := listener.Close(); cerr != nil {
+			log.Println(cerr)
+		}
+	}()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		handle(conn)
+
+	}
+}
+
+func handle(conn net.Conn) {
+	defer func() {
+		if cerr := conn.Close(); cerr != nil {
+			log.Println(cerr)
+		}
+	}()
+
+	reader := bufio.NewReader(conn)
+
+	const delim = '\n'
+	line, err := reader.ReadString(delim)
+
+	if err != nil {
+		if err != io.EOF {
+			log.Println(err)
+		}
+		log.Printf("received: %s\n", line)
+		log.Println(err)
+	}
+	log.Printf("received: %s\n", line)
+
+	path, err := getPath(line)
+	if err != nil {
+		log.Println(err)
+	}
+
+	switch path {
+	case "/":
+		err = writeIndex(conn)
+	case "/transactions.csv":
+		err = writeTransactionsCsv(conn)
+	default:
+		err = write404(conn)
+	}
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func writeResponse(conn net.Conn, status int, strings []string, file []byte) error {
+	writer := bufio.NewWriter(conn)
+
+	_, err := writer.WriteString(fmt.Sprintf("HTTP/1.1 %d", status) + CRLF)
+	if err != nil {
+		return err
+	}
+
+	for i, s := range strings {
+		if len(strings) == (i + 1) {
+			s += CRLF
+		}
+
+		_, err = writer.WriteString(s + CRLF)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if file != nil {
+		_, err = writer.Write(file)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func write404(conn net.Conn) error {
+	return writeResponse(conn, 404, []string{"Connection: close"}, nil)
+}
+
+func writeIndex(conn net.Conn) error {
+	file, err := includeIndexTemplate()
+	if err != nil {
+		return err
+	}
+
+	return writeResponse(conn, 200, []string{
+		"Connection: close",
+		fmt.Sprintf("Content-Length: %d", len(file)),
+		"Content-Type: text/html; charset=utf-8",
+	}, file)
+
+}
+
+func writeTransactionsCsv(conn net.Conn) error {
+	file, err := ioutil.ReadFile("web/shared/transactions.csv")
+	if err != nil {
+		return err
+	}
+
+	return writeResponse(conn, 200, []string{
+		"Content-Type: text/csv",
+		fmt.Sprintf("Content-Length: %d", len(file)),
+		"Connection: close",
+	}, file)
+
+}
+
+func includeIndexTemplate() ([]byte, error) {
+	username := "Michael"
+	balance := "1 000.50"
+
+	file, err := ioutil.ReadFile("web/template/index.html")
+	if err != nil {
+		return nil, err
+	}
+
+	file = bytes.ReplaceAll(file, []byte("{username}"), []byte(username))
+	file = bytes.ReplaceAll(file, []byte("{balance}"), []byte(balance))
+	return file, nil
+}
+
+func getPath(line string) (string, error) {
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return "", errors.New(fmt.Sprintf("invalid request line %s", line))
+	}
+	return parts[1], nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module bgo-homeworks-09
+
+go 1.15
